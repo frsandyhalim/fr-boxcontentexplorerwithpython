050b2d429d775353626e53bb81afc915ce5a77f2
frsandyhalim <45084818+frsandyhalim@users.noreply.github.com>
2018-11-26 14:48:20 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# Folder to display
+fid = '9999999999'
+
+# Auth config
+client_id = 'xxxxxxxx'
+client_secret='yyyyyyyy'
+enterprise_id='xzzzzz'
+jwt_key_id= 'pppppppp'
+private_key_path='private.pem'
+private_key_passphrase='qqqqqqqqqqqqqqq'
---
+++ contentexplorer.py
@@ -0,0 +1,36 @@
+from boxsdk import Client
+from boxsdk import JWTAuth
+from flask import Flask, render_template
+import config
+
+# Configure JWT auth and fetch access token
+auth = JWTAuth(
+  client_id=config.client_id,
+  client_secret=config.client_secret,
+  enterprise_id=config.enterprise_id,
+  jwt_key_id=config.jwt_key_id,
+  rsa_private_key_file_sys_path=config.private_key_path,
+  rsa_private_key_passphrase=config.private_key_passphrase
+)
+
+
+# Obtain client auth
+access_token = auth.authenticate_instance()
+client = Client(auth)
+print access_token
+print config.fid
+
+me = client.user(user_id='me').get(fields=['login'])
+print('The email of the user is: {0}'.format(me['login']))
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+  html = render_template('template.html',
+    fid=config.fid,
+    at=access_token
+  )
+  return html
+
+if __name__ == '__main__':
+  app.run()
